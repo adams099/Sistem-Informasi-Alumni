626bc61b5a75e593bd40786f6e3d47f52dd97a0e
Adam Musyafa <adammusyafa04@gmail.com>
2024-01-08 22:33:43 +0700

    update user guide
    Restore user guide alumni and student in register then move dashboard like admin

--- app/Views/layout/sidebar.php
+++ app/Views/layout/sidebar.php
@@ -266,21 +266,19 @@
         </ul>
     </div>
     <div class="sidenav-footer mx-3 ">
-        <?php if (in_groups('admin')) : ?>
-            <div class="card card-background shadow-none card-background-mask-secondary" id="sidenavCard">
-                <div class="full-background" style="background-image: url('../assets/img/curved-images/white-curved.jpg')"></div>
-                <div class="card-body text-start p-3 w-100">
-                    <div class="icon icon-shape icon-sm bg-white shadow text-center mb-3 d-flex align-items-center justify-content-center border-radius-md">
-                        <i class="ni ni-diamond text-dark text-gradient text-lg top-0" aria-hidden="true" id="sidenavCardIcon"></i>
-                    </div>
-                    <div class="docs-info">
-                        <h6 class="text-white up mb-0">Need help?</h6>
-                        <p class="text-xs font-weight-bold">Check the user guide for details</p>
-                        <a href="<?= site_url('guide-admin'); ?>" target="_blank" class="btn btn-white btn-sm w-100 mb-0">Documentation</a>
-                    </div>
+        <div class="card card-background shadow-none card-background-mask-secondary" id="sidenavCard">
+            <div class="full-background" style="background-image: url('../assets/img/curved-images/white-curved.jpg')"></div>
+            <div class="card-body text-start p-3 w-100">
+                <div class="icon icon-shape icon-sm bg-white shadow text-center mb-3 d-flex align-items-center justify-content-center border-radius-md">
+                    <i class="ni ni-diamond text-dark text-gradient text-lg top-0" aria-hidden="true" id="sidenavCardIcon"></i>
+                </div>
+                <div class="docs-info">
+                    <h6 class="text-white up mb-0">Need help?</h6>
+                    <p class="text-xs font-weight-bold">Check the user guide for details</p>
+                    <a href="<?= in_groups('admin') ? site_url('guide-admin') : site_url('guide-alumni-student') ?>" target="_blank" class="btn btn-white btn-sm w-100 mb-0">Documentation</a>
                 </div>
             </div>
-        <?php endif; ?>
+        </div>
         <a class="btn bg-gradient-primary mt-3 w-100" href="<?= base_url('logout'); ?>">Logout</a>
     </div>
 </aside>
